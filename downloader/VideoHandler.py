652be0a8938e1f1fbc93a64d03000dbe51d6e3de
chenak abdel <chenak.alaa1@gmail.com>
2022-12-26 16:57:22 -0500

    change request lib

--- downloader/VideoHandler.py
+++ downloader/VideoHandler.py
@@ -1,5 +1,4 @@
 from abc import ABC, abstractmethod
-import urllib.request
 import re
 import js2py
 import json
@@ -25,11 +24,9 @@ class AbsHandler(ABC):
         pass
     
     def _fetch(self, url :str):
+       
         
-        req = urllib.request.Request(url)
-        req.add_header('Connection', 'keep-alive')
-        req.add_header('Accept-Ranges', 'bytes')
-        return urllib.request.urlopen(req)
+        return get(url)
            
                 
         
@@ -63,8 +60,6 @@ class Youtube(AbsHandler):
         urlSlice = re.split(r'&',url)
         key = urlSlice[0].replace("s=","")
         decipherKey = self.decription(key,body)
-        print(key)
-        print(decipherKey)
         queryValue = urlSlice[1].replace("sp=","")
         url = urlSlice[2].replace("url=","")
         newURL = urllib.parse.unquote(url)
@@ -78,8 +73,7 @@ class Youtube(AbsHandler):
         self.video = self.searchVideo(steamedData,quality)
         if "url" not in self.video:
             self.video["url"] = self.decipherUrl(self.video["signatureCipher"],body)
-            print(self.video)
-            get(self.video["url"]).content        
+            print(self._fetch(self.video["url"]).status_code)
     
     def getCipherKey(self, body :str) -> str:
         return re.search(r"[\{\d\w\(\)\\.\=\"]*?;(..\...\(.\,..?\)\;){3,}.*?}", body)[0]
@@ -95,14 +89,13 @@ class Youtube(AbsHandler):
         script = value + key 
         script = script.replace("\n","")
         codeCipher = js2py.eval_js(script)
-        print(script)
         return codeCipher(data)
         
     
     def decription(self,data :str,body :str) -> str:
         jsUrl = 'https://youtube.com/'+re.findall(r'"jsUrl":"(.*?)"', body)[0]
         
-        jsBody = self._fetch(jsUrl).read().decode('utf-8')
+        jsBody = self._fetch(jsUrl).text
         
         cipherKey = self.getCipherKey(jsBody)
         
@@ -115,7 +108,8 @@ class Youtube(AbsHandler):
         pass
     def download(self, url :str) -> None:
         
-        mainBody =self._fetch(url).read().decode('utf-8')
+        mainBody =self._fetch(url).text
+        
         self.getSteamedData(mainBody)
         
         #site = "https://rr7---sn-cxaaj5o5q5-t0a6.googlevideo.com/videoplayback?expire=1671972630&ei=tvKnY6mBCKWa6QLz_6vQAg&ip=142.119.12.135&id=o-AMiuejfivWbEP4yKAnfdc5Rmaj79FHYYtRKHy5vqoJbC&itag=135&aitags=133%2C134%2C135%2C136%2C137%2C160%2C242%2C243%2C244%2C247%2C248%2C278&source=youtube&requiressl=yes&mh=Ru&mm=31%2C26&mn=sn-cxaaj5o5q5-t0a6%2Csn-tt1e7nls&ms=au%2Conr&mv=m&mvi=7&pl=22&initcwndbps=1535000&vprv=1&mime=video%2Fmp4&ns=q02iNeHLENRi18bBJwkV2ywK&gir=yes&clen=6086172&dur=166.366&lmt=1576436449756320&mt=1671950710&fvip=2&keepalive=yes&fexp=24001373%2C24007246&c=WEB&txp=2311222&n=5GbRlEy0ZAyE-C-sC&sparams=expire%2Cei%2Cip%2Cid%2Caitags%2Csource%2Crequiressl%2Cvprv%2Cmime%2Cns%2Cgir%2Cclen%2Cdur%2Clmt&sig=AOq0QJ8wRgIhAKOwXMVmt7OwnETsx9DFMSfyMtu44l_8yjcyhciIQggtAiEA_EexzBLr3tuUGKwcRBpxHmUVYT-6jO_wVJ8LiTowi5k%3D&lsparams=mh%2Cmm%2Cmn%2Cms%2Cmv%2Cmvi%2Cpl%2Cinitcwndbps&lsig=AG3C_xAwRAIgDZOHGx9vbazxvb6vHABfqHh_TsTs6znjIIFJsRLWT4MCIEqF9GRRwpw4wc442cFjSYoB5ivw7mkCU7-_YlzSmFwB"
