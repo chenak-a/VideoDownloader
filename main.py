a9dd74ca03d6e41d373d588d9e0462c857622252
chenak abdel <chenak.alaa1@gmail.com>
2022-12-30 23:35:57 -0500

    change video

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ import concurrent.futures
 def main():
     type = Format()
     downloader = VideoDownloader()
-    video = ["https://www.youtube.com/watch?v=WlaGHS_qs58","https://www.youtube.com/watch?v=uN0qoukeZvI&list=PLN68qIdYV64AjOAmJSN3uylEaQzFkq882&index=30","https://www.youtube.com/watch?v=l5zWiMjBQEE&list=PLN68qIdYV64AjOAmJSN3uylEaQzFkq882&index=16"]
-    with concurrent.futures.ThreadPoolExecutor(max_workers = 5) as executor:
+    threadPool = 5
+    video = ["https://www.youtube.com/watch?v=U0cfJJupyGc&ab_channel=Ar-RahmanTv","https://www.youtube.com/watch?v=nlYCyGaXPW0&ab_channel=UnMusulman"]
+    with concurrent.futures.ThreadPoolExecutor(max_workers = threadPool) as executor:
         executor.map(downloader.downloadVideo,video,repeat(type.VIDEO))
-    #for i in video:
-    #    downloader.downloadVideo(i,type.VIDEO)
+
 if __name__ == '__main__':
     main()
