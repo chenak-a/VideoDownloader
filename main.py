8a211769ad3239d8cc19a484b0cdd4eb9896c643
chenak abdel <chenak.alaa1@gmail.com>
2023-01-05 10:29:05 -0500

    optimize audio video merge

--- main.py
+++ main.py
@@ -5,14 +5,15 @@ def main():
     downloader = VideoDownloader()
     video = [
         
-        "https://www.youtube.com/watch?v=IUWJ8_lkFAA",
-        "https://www.youtube.com/watch?v=G8h_2bvkHa0",
-        "https://www.youtube.com/watch?v=F9Zt4IFOvLI",
+        #"https://www.youtube.com/watch?v=IUWJ8_lkFAA",
+        #"https://www.youtube.com/watch?v=G8h_2bvkHa0",
+        #"https://www.youtube.com/watch?v=F9Zt4IFOvLI",
         "https://www.youtube.com/watch?v=AphD9lzWRlE"
     ]
     audio = [
         "https://www.youtube.com/watch?v=BYRsQvPOv6o",
         ]
+    downloader.setDefaultVideoQuality(144)
     downloader.run(video=video, audio=audio)
 
 
